97053c6070853859bb44ded5eb7191ea31230fcb
Kougarz <122490236+Kougarz@users.noreply.github.com>
2025-11-11 17:09:06 -0800

    Fixed Hele-Shaw Window
    Adjusted code to fix Hele-shaw window

--- hele-shaw_canvas.js
+++ hele-shaw_canvas.js
@@ -1,30 +1,35 @@
 function drawHeleShaw(){
+    // create liquid
+    let Water = new Liquid();
+
     let hs_canvas = document.getElementById('hele-shaw_canvas');
     let hs = hs_canvas.getContext('webgl2');
 
-    let vertex_source = `#version 300 es
-    precision mediump float;
+    let vertex_source = 
+        `#version 300 es
+        precision mediump float;
 
-    in vec3 coordinates;
-    in vec4 color;
+        in vec3 coordinates;
+        in vec4 color;
 
-    out vec4 v_color;
+        out vec4 v_color;
 
-    void main(void) { 
-        gl_Position = vec4(coordinates, 1.0);
-        v_color = color;
-    }`
+        void main(void) { 
+            gl_Position = vec4(coordinates, 1.0);
+            v_color = color;
+        }`;
 
-    let fragment_source = `#version 300 es
-    precision mediump float;
+    let fragment_source = 
+        `#version 300 es
+        precision mediump float;
 
-    in vec4 v_color;
+        in vec4 v_color;
 
-    out vec4 f_color;
+        out vec4 f_color;
 
-    void main(void){
-        f_color = v_color;
-    }`
+        void main(void){
+            f_color = v_color;
+        }`;
 
     // create program
     let program = hs.createProgram() 
@@ -47,16 +52,20 @@ function drawHeleShaw(){
     // select program
     hs.useProgram(program);
 
+    // bind indices
+    hs.bindBuffer( hs.ELEMENT_ARRAY_BUFFER, Water.createAndLoadIndices(hs));
+
     // defaults
     hs.clearColor( 100/255.0, 100/255.0, 100/255.0, 1.0 );
     hs.enable( hs.DEPTH_TEST );
     hs.enable( hs.BLEND );
 
-    // create liquid
-    let Water = new Liquid();
 
-    let current_array_buf = 0;
-    let attrib_location = 0;
+    // add some sinks and sources
+    Water.addSink(-0.5, 0.5);
+    Water.addSink(-0.5, -0.5);
+    Water.addSource(0.75, 0.5);
+    Water.addSource(0.75, -0.25);
 
     // Render loop
     function render(now){
@@ -66,12 +75,9 @@ function drawHeleShaw(){
         // clear previous window
         hs.clear(hs.COLOR_BUFFER_BIT | hs.DEPTH_BUFFER_BIT);
 
-        // bind indices
-        hs.bindBuffer( hs.ELEMENT_ARRAY_BUFFER, Water.createAndLoadIndices(hs));
-
         // bind coordinates
-        attrib_location = hs.getAttribLocation(program, "coordinates");
-        current_array_buf = hs.getParameter(hs.ARRAY_BUFFER_BINDING);
+        let attrib_location = hs.getAttribLocation(program, "coordinates");
+        let current_array_buf = hs.getParameter(hs.ARRAY_BUFFER_BINDING);
         hs.bindBuffer(hs.ARRAY_BUFFER, Water.createAndLoadVertex(hs));
         hs.enableVertexAttribArray(attrib_location);
         hs.vertexAttribPointer(attrib_location, 3, hs.FLOAT, false, 28, 0);
@@ -86,7 +92,7 @@ function drawHeleShaw(){
         hs.bindBuffer(hs.ARRAY_BUFFER, current_array_buf);
 
         // draw lines
-        hs.drawElements( hs.LINE_STRIP, Water.getIndices.length, hs.UNSIGNED_SHORT, 0 );
+        hs.drawElements( hs.LINE_STRIP, Water.getIndices().length, hs.UNSIGNED_SHORT, 0 );
     }
     window.requestAnimationFrame(render);
 }
--- liquid.js
+++ liquid.js
@@ -18,35 +18,29 @@ class Liquid {
         this.#vert_offset = 0; // index of first vertex value
 
         // 100 x 100 grid point in a window going from -1 to 1 for both x and y
-        const rows = 100;
-        const cols = 100;
-        const left = -1.0;
-        const right = 1.0;
-        const bot = -1.0;
-        const top = 1.0;
+        const rows = 50;
+        const cols = 1000;
+        const left = -1.1;
+        const right = 1.1;
+        const bot = -1.1;
+        const top = 1.1;
 
         // spacing based on above info
-        const row_spacing = (right - left) / (rows - 1);
-        const col_spacing = (top - bot) / (cols - 1);
+        const row_spacing = (right - left) / (cols - 1);
+        const col_spacing = (top - bot) / (rows - 1);
 
         // set verts and indices
-        this.#verts = [
-            -0.5, 0.5, 0, 0, 0, 1, 1,
-            0.5, 0.5, 0, 0, 0, 1, 1,
-            0.5, -0.5, 0, 0, 0, 1, 1,
-            -0.5, -0.5, 0, 0, 0, 1, 1,
-            -0.5, 0.5, 0, 0, 0, 1, 1,
-        ];
-        this.#indices = [0, 1, 2, 3];
-        // for (let row = 0; row < rows; row++) {
-        //     for (let col = 0; col < cols; col++) {
-        //         let x = col * row_spacing + left;
-        //         let y = top - row * col_spacing;
-        //         this.#verts.push(x, y, 0, 0, 0, 1, 1); // z = 0, RGBA = Blue
-        //         this.#indices.push(row * cols + col); 
-        //     }
-        //     this.#indices.push(0xFFFF); // snip the indices gl.LINE_STRIP
-        // }
+        this.#verts = [];
+        this.#indices = [];
+        for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+                let x = col * row_spacing + left;
+                let y = top - row * col_spacing;
+                this.#verts.push(x, y, 0, 0, 0, 1, 1); // z = 0, RGBA = Blue
+                this.#indices.push(row * cols + col); 
+            }
+            this.#indices.push(0xFFFF); // snip the indices gl.LINE_STRIP
+        }
     }
 
     // Getters
@@ -72,7 +66,7 @@ class Liquid {
 
     // Class methods
     addSink(x, y) {
-        const mag = 0.001;
+        const mag = 0.1;
         // iterate over all of the verts
         for (let vert = 0; vert < this.#verts.length; vert += this.#stride) {
             let x_dif = x - this.#verts[vert];
@@ -81,14 +75,16 @@ class Liquid {
             let rad = Math.atan2(y_dif, x_dif); // angel of force
             let distance = Math.sqrt(x_dif * x_dif + y_dif * y_dif);
             // inverse square of distance to calculate force
-            let force = 1 * mag / distance * distance;
+            let force = mag / distance / distance;
+            if (force > mag) force = mag;
+            if (force > distance) force = distance;
             this.#verts[vert] += force * Math.cos(rad);
-            this.#verts[vert] += force * Math.sin(rad);
+            this.#verts[vert+1] += force * Math.sin(rad);
         }
     }
 
     addSource(x, y) {
-        const mag = 0.001;
+        const mag = 0.1;
         // iterate over all of the verts
         for (let vert = 0; vert < this.#verts.length; vert += this.#stride) {
             let x_dif = this.#verts[vert] - x;
@@ -97,9 +93,10 @@ class Liquid {
             let rad = Math.atan2(y_dif, x_dif); // angel of force
             let distance = Math.sqrt(x_dif * x_dif + y_dif * y_dif);
             // inverse square of distance to calculate force
-            let force = 1 * mag / distance * distance; 
+            let force = mag / distance / distance;
+            if (force > mag) force = mag; 
             this.#verts[vert] += force * Math.cos(rad);
-            this.#verts[vert] += force * Math.sin(rad);
+            this.#verts[vert+1] += force * Math.sin(rad);
         }
     }
 
