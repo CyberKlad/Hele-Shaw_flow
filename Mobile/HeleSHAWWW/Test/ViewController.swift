fd60e06013e7936ffb7ed9ec4ee2ceb749cba79f
Adem <ademosmancevic7@gmail.com>
2026-03-23 09:57:36 -0700

    Button Basic Functionality
    Play and clear should be working (pause WIP)
    
    Source and Sink should be working from Kolton's most recent PR
    
    I'm not sure if the streamlines are correct, and I am using the

--- Mobile/HeleSHAWWW/Test/ViewController.swift
+++ Mobile/HeleSHAWWW/Test/ViewController.swift
@@ -5,11 +5,25 @@
 //  Created by Oscar Langarica on 2/1/26.
 //
 
+enum PlaceMode {
+    case none
+    case sink
+    case source
+}
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var gridView: HeleShawGrid!
+    @IBOutlet weak var sourceButton: UIButton!
+    @IBOutlet weak var sinkButton: UIButton!
+    @IBOutlet weak var playpauseButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    var isPaused = true
+    var placeMode: PlaceMode = .none
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,10 +31,68 @@ class ViewController: UIViewController {
         gridView.rows = 8
         gridView.radius = 5
         gridView.setNeedsDisplay()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(gridPress(_:)))
+        gridView.addGestureRecognizer(tapGesture)
+        gridView.isUserInteractionEnabled = true
+    }
     
+    
+    @IBAction func sourceButtonPressed(_ sender: Any) {
+        print("source PRESSED")
+        placeMode = .source
+    }
+    
+    @IBAction func playButtonPressed(_ sender: UIButton){
+        isPaused.toggle()
+        if isPaused {
+            sender.setTitle("Play", for:.normal)
+        }
+        else {
+            sender.setTitle("Pause", for:.normal)
+        }
+        gridView.isPaused = isPaused
+        gridView.setNeedsDisplay()
+    }
+    
+    @IBAction func sinkButtonPressed(_ sender: Any) {
+        print("sink pressed")
+        placeMode = .sink
         
     }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        print("clear pressed")
+        gridView.sources.removeAll()
+        gridView.sinks.removeAll()
+        gridView.setNeedsDisplay()
+    }
 
+    
+    @objc func gridPress(_ gesture: UITapGestureRecognizer) {
+        let point = gesture.location(in: gridView)
+        let x_space = gridView.bounds.width / CGFloat(gridView.cols + 1)
+        let y_space = gridView.bounds.height / CGFloat(gridView.rows + 1)
+        let col = Int(round(point.x / x_space)) - 1
+        let row = Int(round(point.y / y_space)) - 1
+        if(row < 0 || row >= gridView.rows || col < 0 || col >= gridView.cols){
+            return
+        }
+        
+        let place = gridView.gridPoint(col: col, row: row)
+        let x = place.0
+        let y = place.1
+        
+        //let x = Float64(point.x)
+       // let y = Float64(point.y)
+        if(placeMode == .sink){
+            gridView.sinks.append((x,y, 6.0))
+        }
+        else if(placeMode == .source){
+            gridView.sources.append((x,y, -6.0))
+        }
+        gridView.setNeedsDisplay()
+    }
     @IBAction func RemoveLater(_ sender: Any) {
         let sinks: [(Float64, Float64, Float64)] = [];
         let sources: [(Float64, Float64, Float64)] = [(2.0, 4.0, -2.0), (4.0, 5.0, -1.0), (1.0,2.0,1.0), (4.0, 3.0, 2.0)];
